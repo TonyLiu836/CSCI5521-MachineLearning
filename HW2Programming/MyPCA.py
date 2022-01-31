e547e4110f064431efff9d6435808cc9598c49ec
Tony Liu <31741598+TonyLiu836@users.noreply.github.com>
2022-01-31 10:42:55 -0600

    Update MyPCA.py

--- HW2Programming/MyPCA.py
+++ HW2Programming/MyPCA.py
@@ -9,18 +9,8 @@ def PCA(X,num_dim=None):
     # finding the projection matrix that maximize the variance (Hint: for eigen computation, use numpy.eigh instead of numpy.eig)
     
     lamb, vect = np.linalg.eigh(cov)
-    #print("shape of lambda",len(lamb))
-    #print("shpae of eigenvect matrix", np.shape(vect))
-    
-    #PCAs = []
-    #lamb = lamb.sort()
-    #print("lambda", lamb)
-    # select the reduced dimensions that keep >95% of the variance
-    #print("bool of num_dim", num_dim is None)
-    #print("num_dim=", num_dim)
-    #print("vect[0][0]",vect[0][0])
+
     if num_dim is None:
-        #print("code reaches here")
         diagSum= 0
         var = 0
         for i in range(len(lamb)):
@@ -32,28 +22,21 @@ def PCA(X,num_dim=None):
         vect = vect[:,idx]
         ratio = var / diagSum
         while ratio < .95:
-            #for i in range(np.shape(lamb)[0]):
-            #    var += vect[i][i]
-            #    var = var/diagSum
             var += lamb[dim]
             ratio = var/diagSum
             dim += 1
-            #print(var)
-            #var = var/diagSum 
-        #print("index=",dim)
-        #print("var=", var)
-        #print("diagSum", diagSum)
+
         w = vect[:,0:dim]
-        #print("shape of w", np.shape(w))
+
         X_pca = np.dot(Xcentered, w)
         num_dim = dim
+        
     # project the high-dimensional data to low-dimensional one
     else:
         maxEigValIndex = np.argmax(lamb)
-        #print("shape of eigenvect matrix", np.shape(vect))
+        
         w = vect[:,maxEigValIndex]
-        #print("shape of w", np.shape(w))
+        
         X_pca = np.dot(Xcentered, np.transpose([w]))
-        #print(np.shape(X_pca))
-        #X_pca = np.transpose([X_pca])
+        
     return X_pca, num_dim
